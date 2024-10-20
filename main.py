fc6602fe8e797af11c5bf485b78ee554d8f541e2
qhhadest <mishz31ytrdaw@gmail.com>
2024-10-20 15:01:49 +0200

    test.py ДЗ

--- main.py
+++ main.py
@@ -4,66 +4,68 @@ import random
 
 class Student:
         def __init__(self, name, age):
-        # public prop
-        self.name = name
-        self.age = age
-        self.gladness = 50
-        self.alive = True
-        self.progress = 0
-        # private
-        self.__money = 100
+            # public prop
+            self.name = name
+            self.age = age
+            self.gladness = 50
+            self.alive = True
+            self.progress = 0
+            # private
+            self.__money = 100
 
-    def show_info(self):
-        print(self.name)
-        print(self.age)
-        print(self.__money, "$")
+        def show_info(self):
+            print(self.name)
+            print(self.age)
+            print(self.__money, "$")
 
-    def to_study(self):
-        print("time to study")
-        self.gladness -= 3
-        self.progress += 3
+        def to_study(self):
+            print("time to study")
+            self.gladness -= 3
+            self.progress += 3
 
-    def to_sleep(self):
-        print("i need to sleep")
-        self.gladness += 5
+        def to_sleep(self):
+            print("i need to sleep")
+            self.gladness += 5
 
-    def to_chill(self):
-        print("rest time")
-        self.progress -= 0.1
-        self.gladness += 5
+        def to_chill(self):
+            print("rest time")
+            self.progress -= 0.1
+            self.gladness += 5
 
-    def is_alive(self):
-        if self.progress < -0.5:
-            print("cast out")
-            self.alive = False
-        elif self.gladness <= 0:
-            print("depression")
-            self.alive = False
+        def is_alive(self):
+            if self.progress < -0.5:
+                print("cast out")
+                self.alive = False
+            elif self.gladness <= 0:
+                print("depression")
+                self.alive = False
 
-
-    def end_of_day(self):
-        print(f"name {self.name}")
-        print(f"gladness {self.gladness}")
-        print(f"progress {self.progress}")
-
-    def live(self,day):
-        day = "Day" +str(day) + "of" + self.name + "life"
-        print(day)
-        live_cube = random.randint(1,3)
-        if live_cube == 1:
-           self.to_study()
-        elif live_cube == 2:
-            self.to_chill()
-        elif live_cube == 3:
-            self.to_sleep()
-            self.end_of_day()
-            self.is_alive()
+        def work(self):
+            if self.__money >= 90:
+                self.gladness -= 10
+                self.__money += 20
 
 
-class Pet:
-        def __init__(self, name, age, type):
-        # public prop
-        self.name()
+        def end_of_day(self):
+            print(f"name {self.name}")
+            print(f"gladness {self.gladness}")
+            print(f"progress {self.progress}")
+
+        def live(self,day):
+            day = "Day" +str(day) + "of" + self.name + "life"
+            print(day)
+            live_cube = random.randint(1,3)
+            if live_cube == 1:
+               self.to_study()
+            elif live_cube == 2:
+                self.to_chill()
+            elif live_cube == 3:
+                self.to_sleep()
+                self.end_of_day()
+                self.is_alive()
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,1 +1,79 @@
-print('Hello')
+import random
+
+
+class Pet:
+    pass
+    def __init__(self, name, age, type):
+        # public prop
+        self.name = name
+        self.age = age
+        self.type = type
+        self.gladness = 50
+        self.need = 20
+        self.alive = True
+
+    def show_info(self):
+        print(self.name)
+        print(self.age)
+        print(self.type)
+
+    def take_a_walk(self):
+        print("Time to a take a walking!")
+        self.gladness += 5
+        self.need -= 10
+
+    def to_sleep(self):
+        print("I want to sleep")
+        self.gladness += 2
+        self.need += 5
+
+    def is_alive(self):
+        if self.gladness == 0
+            print("Depression")
+            self.alive = False
+
+    def to_chill(self):
+        print("rest time")
+        self.gladness += 5
+
+    def to_eat(self):
+        print("time for eating")
+        self.gladness += 3
+        self.need += 8
+
+    def end_of_day(self):
+        print(f"name{self.name}")
+        print(f"gladness{self.gladness}")
+        print(f"need{self.need}")
+        print(f"alive"{self.alive})
+
+
+    def live(self,day):
+        day = "Day" +str(day) + "of" + self.name + "life"
+        print(day)
+        live_cube = random.randint(1,3)
+        if live_cube == 1:
+           self.take_a_walk()
+        elif live_cube == 2:
+            self.to_chill()
+        elif live_cube == 3:
+            self.to_eat()
+            self.to_sleep()
+            self.end_of_day()
+            self.is_alive()
+
+
+
+nick = Pet("Cat",3)
+for day in range(365):
+    if nick.alive == False:
+        break
+    nick.live(day)
+
+
+
+st1 = Pet("Cat", 3)
+st1.show_info()
+
+st2 = Pet("Dog", 5)
+st2.show_info()

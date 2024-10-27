95153e6b2d63955d36320ac6a34f7ec7b9d63bce
qhhadest <mishz31ytrdaw@gmail.com>
2024-10-27 17:26:52 +0100

    cw3 upd for dz3

--- cw/cw3.py
+++ cw/cw3.py
@@ -1,7 +1,8 @@
 class Human:
-    def __init__(self, name="Human"):
+    def __init__(self, name="Human", gladness):
+        self.gladness = gladness
         self.name = name
-
+        self.gladness = 50
 
 class Auto:
     def __init__(self,brand):
@@ -12,16 +13,28 @@ class Auto:
         self.passanger.append(human)
 
     def print_passanger(self):
-        if self.passanger = []
+        if self.passanger == []:
            print(f"Names of {self.brand} passanger:")
            for passanger in self.passanger:
                print(passanger.name)
         else:
             print(f"No passanger in {self.brand}")
 
+class Pet(Human):
+    def __init__(self, pet, type, age, gladness):
+        self.age = age
+        self.pet = pet
+        self.type = type
+        self.gladness = gladness
+
+    def fun(self):
+        self.gladness += 10
+
+
 
 james = Human("James")
 amelia = Human("Amelia")
+
 car = Auto("BMW")
 car.add_passanger(james)
 car.add_passanger(amelia)
